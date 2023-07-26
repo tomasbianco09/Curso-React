2fa9653ad703e6762aaf91a2e0705042863729c8
tbianco <tomibianco009@gmail.com>
2023-07-26 20:35:27 -0300

    ultimos cambios en item

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
-import { Card, CardHeader, Heading, CardBody, Text, CardFooter, Button } from '@chakra-ui/react'
+import { } from '@chakra-ui/react'
 
-const Item = ({nombre, description, stock}) => {
+const Item = ( nombre, description,) => {
   return (
-    <>
-      <Card>
-        <CardHeader>
-          <Heading size='md'> {nombre} </Heading>
-        </CardHeader>
-        <CardBody>
-          <Text> {description} </Text>
-        </CardBody>
-        <CardFooter>
-          <Button>Agregar al carrito</Button>
-        </CardFooter>
-      </Card>
-    </>
+    <div>
+          <div className="card">
+                  <div className="product-image">
+                      {/* <img src="/img/prod/AST 2223 CSGO PRO JERSEY.jpg" alt="Imagen de cama"/>
+                      <img src="/img/prod/AST 2223 CSGO PRO JERSEY BACK.jpg" alt="Imagen de cama" className="hover-img"/> */}
+                  </div>
+                  <div className="product-info">
+                      <h4>
+                        {nombre}
+                      </h4>
+                      <p>{description}</p>
+                      <p></p>
+                  </div>
+      </div>
+    </div>
   )
 }
 
