de95f68e81667741322e7c49b5f95f85ca4c43e8
tbianco <tomibianco009@gmail.com>
2023-07-26 21:46:47 -0300

    agregando estilo en item

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,25 +1,22 @@
 import React from 'react'
-import { } from '@chakra-ui/react'
+import { Card, CardHeader, Heading, CardBody, Text, CardFooter, Button } from '@chakra-ui/react'
 
 const Item = ({nombre, description, stock}) => {
   return (
-        
-          
-          
-            <div className="card">
-                <div className="product-image">
-                    {/* <img src="/img/prod/AST 2223 CSGO PRO JERSEY.jpg" alt="Imagen de cama"/>
-                    <img src="/img/prod/AST 2223 CSGO PRO JERSEY BACK.jpg" alt="Imagen de cama" className="hover-img"/> */}
-                </div>
-                <div className="product-info">
-                    <h4>
-                      {nombre}
-                    </h4>
-                    <p>{description}</p>
-                    <p>{stock}</p>
-                </div>
-            </div>
-          
+    <>
+      <Card>
+        <CardHeader>
+          <Heading size='md'> {nombre} </Heading>
+        </CardHeader>
+        <CardBody>
+          <Text> {description} </Text>
+          <Text> {stock} </Text>
+        </CardBody>
+        <CardFooter>
+          <Button>Agregar al carrito</Button>
+        </CardFooter>
+      </Card>
+    </>
   )
 }
 
