65422a4b0bde7a22351ebd29244226ab201cea5f
tbianco <tomibianco009@gmail.com>
2023-07-26 21:21:28 -0300

    ultimo cambio

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
 import { } from '@chakra-ui/react'
 
-const Item = ( nombre, description,) => {
+const Item = ({nombre, description, stock}) => {
   return (
-    <div>
-          <div className="card">
-                  <div className="product-image">
-                      {/* <img src="/img/prod/AST 2223 CSGO PRO JERSEY.jpg" alt="Imagen de cama"/>
-                      <img src="/img/prod/AST 2223 CSGO PRO JERSEY BACK.jpg" alt="Imagen de cama" className="hover-img"/> */}
-                  </div>
-                  <div className="product-info">
-                      <h4>
-                        {nombre}
-                      </h4>
-                      <p>{description}</p>
-                      <p></p>
-                  </div>
-      </div>
-    </div>
+        
+          
+          
+            <div className="card">
+                <div className="product-image">
+                    {/* <img src="/img/prod/AST 2223 CSGO PRO JERSEY.jpg" alt="Imagen de cama"/>
+                    <img src="/img/prod/AST 2223 CSGO PRO JERSEY BACK.jpg" alt="Imagen de cama" className="hover-img"/> */}
+                </div>
+                <div className="product-info">
+                    <h4>
+                      {nombre}
+                    </h4>
+                    <p>{description}</p>
+                    <p>{stock}</p>
+                </div>
+            </div>
+          
   )
 }
 
