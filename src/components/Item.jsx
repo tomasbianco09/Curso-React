1b0857f8b78dab39a97cfa407b588d422ea88b62
tbianco <tomibianco009@gmail.com>
2023-08-28 14:13:21 -0300

    Modificaciones en item  card

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -5,22 +5,20 @@ import { Link } from 'react-router-dom'
 const Item = ({ nombre, description, id, image, precio }) => {
     return (
         <>
-            <Card margin='5px'>
-                <div className='card'>
-                    <div className='product-image'>
-                        <img src={image} alt="" />
-                    </div>
-                    <div className='product-info'>
-                        <h4>{nombre}</h4>
-                        <p>$ {precio}</p>
-                    </div>
-                    <Link to={`/item/${id}`}>
-                        <button className='btn-p'>
-                            <h2>Details</h2>
-                        </button>
-                    </Link>
+            <div className='card'>
+                <div className='product-image'>
+                    <img src={image} alt="" />
                 </div>
-            </Card>
+                <div className='product-info'>
+                    <h4>{nombre}</h4>
+                    <p>$ {precio}</p>
+                </div>
+                <Link to={`/item/${id}`}>
+                    <button className='btn-p'>
+                        <h2>Details</h2>
+                    </button>
+                </Link>
+            </div>
         </>
     )
 }
